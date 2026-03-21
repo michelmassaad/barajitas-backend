5a7d8c75664947656b7949e5d6501b5746e5f217
Michel Massaad <michel_01ft@hotmail.com>
2026-03-20 21:26:43 -0300

    feat: actualizacion del link de productos

--- packages/barajitas-frontend/js/productos.js
+++ packages/barajitas-frontend/js/productos.js
@@ -1,7 +1,7 @@
 let barraBusqueda = document.getElementById("barraBusqueda");
 let contenedorProductos = document.getElementById("contenedor-productos");
 
-const url = "/api/productos/activos"; // Guardamos en una variable la url de nuestro endpoint de productos
+const url = "https://barajitas-backend-production.up.railway.app/api/productos/activos"; // Guardamos en una variable la url de nuestro endpoint de productos
 
 let productos = [];
 
